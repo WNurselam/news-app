dc386c685499ac361cb28472638271f19475652e
Nurselam <77880259+WNurselam@users.noreply.github.com>
2023-04-07 14:22:17 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="App">
+     <div className='text-yellow-300'>News APP</div>
+     
+    </div>
+  )
+}
+
+export default App
