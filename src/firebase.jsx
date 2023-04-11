0f9b70421bd192e347b392f3c54a5e6f7e6ba590
mertyldrr <mertyildirir14@gmail.com>
2023-04-12 00:04:20 +0200

    fix firebase non-serializable response

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,48 +1,58 @@
 import { initializeApp } from "firebase/app";
-import {getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut} from 'firebase/auth'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { toast } from "react-hot-toast";
 
-
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBYJ6q8xVJay-jAEuinKA-xalwPzWPsKEc",
-    authDomain: "news-app-c7cba.firebaseapp.com",
-    projectId: "news-app-c7cba",
-    storageBucket: "news-app-c7cba.appspot.com",
-    messagingSenderId: "759173888750",
-    appId: "1:759173888750:web:1d3f596bd7ad97cf70d1ff"
+  apiKey: "AIzaSyBYJ6q8xVJay-jAEuinKA-xalwPzWPsKEc",
+  authDomain: "news-app-c7cba.firebaseapp.com",
+  projectId: "news-app-c7cba",
+  storageBucket: "news-app-c7cba.appspot.com",
+  messagingSenderId: "759173888750",
+  appId: "1:759173888750:web:1d3f596bd7ad97cf70d1ff",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
-export const register = async (email,password) => {
-    try {
-        const {user} = await createUserWithEmailAndPassword(auth,email,password)
-        return user;
-    } catch (error) {
-        toast.error(error.message);
-    }
-   
-}
+export const register = async (email, password) => {
+  try {
+    const { user } = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return user;
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
 
-export const login = async (email,password) => {
-    try {
-        const {user} = await signInWithEmailAndPassword(auth,email,password);
-        return user;
-    } catch (error) {
-        toast.error(error.message);    
-    }
-
-}
+export const login = async (email, password) => {
+  try {
+    const userCredentials = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredentials.user);
+    return userCredentials.user.toJSON();
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
 
 export const logout = async () => {
-    try {
-        await signOut()
-        return true
-    } catch (error) {
-        toast.error(error.message);    
-    }
-}
+  try {
+    await signOut();
+    return true;
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
 
 export default app;
--- src/store/reducers/Auth.jsx
+++ src/store/reducers/Auth.jsx
@@ -1,22 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : false;
+const user = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : false;
 const initialState = { user };
 
 const auth = createSlice({
-    name:'auth',
-    initialState,
-    reducers:{   
-        login:(state,action) => {
-            localStorage.setItem('user',JSON.stringify(action.payload)),
-            state.user = action.payload
-        },
-        logout:state => {
-            localStorage.removeItem('user')
-            state.user=false
-        }
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      const userObject = action.payload;
+      localStorage.setItem("userId", userObject.uid);
+      state.user = userObject;
+    },
+    logout: (state) => {
+      localStorage.removeItem("userId");
+      state.user = false;
+    },
+  },
+});
 
-export const {login,logout} = auth.actions;
+export const { login, logout } = auth.actions;
 export default auth.reducer;
