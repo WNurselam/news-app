7b2d2378062c93140527badefb9916fe5da3b7dd
Nurselam <77880259+WNurselam@users.noreply.github.com>
2023-04-15 23:40:00 +0300

    edit news dates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,10 +11,16 @@ const Home = () => {
   );
   //console.log(data?.articles)
 
+  const formatDate = (dateStr) => {
+    const date = new Date(dateStr);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  };
+
 
   return (
       <div className="container m-5 mt-0  bg-gray-50 flex ml-52">
-        <div className="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-3 p-14 bg-slate-100 ">
+        <div className="grid grid-cols-1  w-11/12 gap-6 mb-6 lg:grid-cols-3 p-14 bg-slate-100 ">
           {
             data?.articles.map((item, index) => {
               return (
@@ -25,6 +31,10 @@ const Home = () => {
                       src={item.urlToImage}
                       alt="Mountain landscape"
                     />
+                    <span className="flex justify-end mr-4 text-slate-500">
+                      {
+                       formatDate(item.publishedAt)
+                      }</span>
                   </Link>
                   <div className="p-4">
                     <p className="font-bold  mb-2">
