b1fb96b0560dd980c276025ddf5fc916fe716654
Nurselam <77880259+WNurselam@users.noreply.github.com>
2023-04-16 23:39:33 +0300

    change home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,30 +19,37 @@ const Home = () => {
 
 
   return (
-    <div className="m-5 mt-0 bg-gray-50 flex ml-4">
-    <div className="grid grid-cols-1 w-full lg:grid-cols-3 gap-6 mb-6 p-4 bg-slate-100">
-      {data?.map((item) => {
-        return (
-          <div key={item._id} className="bg-white shadow-md rounded-md overflow-hidden">
-            <Link to={item.link}>
-              <img
-                className="h-48 w-full object-cover p-5 rounded-md"
-                src={item.media}
-                alt="Mountain landscape"
-              />
-              <span className="flex justify-end mr-4 text-slate-500">{formatDate(item.published_date)}</span>
-            </Link>
-            <div className="p-4">
-              <p className="font-bold mb-2">
-                <span>{item.author}</span>
-              </p>
-              <b className="text-gray-700 leading-snug">{item.title}</b>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  </div>
+      <div className="container m-5 mt-0  bg-gray-50 flex ml-52">
+        <div className="grid grid-cols-1  w-11/12 gap-6 mb-6 lg:grid-cols-3 p-14 bg-slate-100 ">
+          {
+            data?.map((item) => {
+              return (
+                <div key={item._id} className="bg-white shadow-md rounded-md overflow-hidden ">
+                  <Link to={item.link}>
+                    <img
+                      className="h-48 w-full object-cover p-5 rounded-md"
+                      src={item.media}
+                      alt="Mountain landscape"
+                    />
+                    <span className="flex justify-end mr-4 text-slate-500">
+                      {
+                       formatDate(item.published_date)
+                      }</span>
+                  </Link>
+                  <div className="p-4">
+                    <p className="font-bold  mb-2">
+                      <span>{item.author}</span>
+                    </p>
+                    <b className="text-gray-700 leading-snug">
+                      {item.title}
+                    </b>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
   );
 };
 
