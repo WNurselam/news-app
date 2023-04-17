6d4d68099a20428a2905d872863852ebedfd70e5
Nurselam SAYAN <77880259+WNurselam@users.noreply.github.com>
2023-04-17 13:05:43 +0300

    Update fetchApi.jsx

--- src/api/fetchApi.jsx
+++ src/api/fetchApi.jsx
@@ -6,7 +6,7 @@ export const fetchApi = async (category) => {
     `https://api.newscatcherapi.com/v2/search?q=${category}&lang=en&sort_by=relevancy&page=1&page_size=30`,
     {
       headers: {
-        "x-api-key": "s8qh2aw9G9IsI_sKlD6LJ66pJHRPimA9QiTHgv1Xy0w",
+        "x-api-key": "Xh-KSTnpAlsHqVvFm7xiHGubrmWv95N7e5_mdLrEjes",
       },
     }
   );
